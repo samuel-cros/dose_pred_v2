ecef46e433ebbfa2cae1681d3121c779aa6c3430
samuel-cros <samuelcros81@gmail.com>
2021-09-24 14:26:13 -0400

    Updated for latest metrics

--- data_generation/generate_metrics_on_gt.py
+++ data_generation/generate_metrics_on_gt.py
@@ -24,22 +24,43 @@ import csv
 # get_biggest_tv
 # - goal: get ptv, ctv or gtv (in that order) depending on availability
 # - input: input for a given patient where 
+#       = CHUM
 #       - input[:, :, :, 1] is the ptv 1
 #       - input[:, :, :, 2] is the ctv 1
 #       - input[:, :, :, 3] is the gtv 1
+#       = OpenKBP
+#       - input[:, :, :, 10] is the ptv70
+#       - input[:, :, :, 9] is the ptv63
+#       - input[:, :, :, 8] is the ptv56
 # - output: the available tv mask
-def get_biggest_tv(input_data):
-    if input_data[:, :, :, 1].any():
-        return input_data[:, :, :, 1]
-    else:
-        if input_data[:, :, :, 2].any():
-            return input_data[:, :, :, 2]
+def get_biggest_tv(input_data, dataset):
+    
+    if dataset == 'CHUM':
+        if input_data[:, :, :, 1].any():
+            return input_data[:, :, :, 1], 1
         else:
-            if input_data[:, :, :, 3].any():
-                return input_data[:, :, :, 3]
+            if input_data[:, :, :, 2].any():
+                return input_data[:, :, :, 2], 1
             else:
-                raise ValueError("Input should include at least one of " + \
-                    "the following: ptv 1, ctv 1 or gtv 1.")
+                if input_data[:, :, :, 3].any():
+                    return input_data[:, :, :, 3], 1
+                else:
+                    raise ValueError("Input should include at least one of " + \
+                        "the following: ptv 1, ctv 1 or gtv 1.")
+    elif dataset == 'OpenKBP':
+        if input_data[:, :, :, 10].any():
+            return input_data[:, :, :, 10], 70
+        else:
+            if input_data[:, :, :, 9].any():
+                return input_data[:, :, :, 9], 63
+            else:
+                if input_data[:, :, :, 8].any():
+                    return input_data[:, :, :, 8], 56
+                else:
+                    raise ValueError("Input should include at least one of " + \
+                        "the following: ptv70, ptv63 or ptv56.")
+    else:
+        raise ValueError("Unknown dataset. Handled datasets are CHUM and OpenKBP")
 
 ###############################################################################
 ## Args
@@ -49,14 +70,34 @@ parser = argparse.ArgumentParser(description='Generate metrics results on ground
 
 # Arguments
 parser.add_argument('-set', '--kind_of_set', type=str, required=True)
+parser.add_argument('-dset', '--dataset', type=str, required=True)
 
 # Additional defaults
 #parser.set_defaults()
 args = parser.parse_args()
 
-# Load dataset
-dataset = h5py.File(os.path.join('..', '..', 'data', 
-                                 'dataset_resized_rd_summed.h5'), 'r')
+# Load dataset and other setups
+if args.dataset == 'CHUM':
+    dataset = h5py.File(os.path.join('..', '..', 'data', 
+                                    'dataset_resized_rd_summed.h5'), 'r')
+    seed_range = 4
+elif args.dataset == 'OpenKBP':
+    if args.kind_of_set == 'validation':
+        dataset = h5py.File(os.path.join('..', 
+                                        '..', 
+                                        '..', 
+                                        'shared',
+                                        'dataset_validation2'), 'r')
+    elif args.kind_of_set == 'test':
+        dataset = h5py.File(os.path.join('..', 
+                                        '..',
+                                        '..', 
+                                        'shared',
+                                        'dataset_test2'), 'r')
+    else:
+        raise ValueError("Unknown dataset. Handled datasets are CHUM and OpenKBP")
+        
+    seed_range = 2
 
 # Setup paths
 path_to_results = 'metrics_on_gt'
@@ -64,23 +105,25 @@ Path(path_to_results).mkdir(parents=True, exist_ok=True)
 
 # Setup CSV
 metrics_gt_csv = open(os.path.join(path_to_results, 
-                                   'metrics_gt_' + args.kind_of_set + '.csv'), 
+                                   'metrics_gt_' + args.kind_of_set + '_' + \
+                                       args.dataset + '.csv'), 
                             'w',
                             newline='')
-smax_fields = ['Smax ' + str(i) for i in range(1, 21)]
-smean_fields = ['Smean ' + str(i) for i in range(1, 21)]
-fields = ['ID', 'D99', 'D98', 'D95', 'Dmax'] + smax_fields + \
-    smean_fields + ['H1', 'H2']
+fields = ['ID', 'HI', 'H2', 'D99', 'D98', 'D95', 'Dmax', 'CI', "van't Riet", 'R50']
 metrics_gt_writer = csv.DictWriter(metrics_gt_csv, fieldnames=fields)
 metrics_gt_writer.writeheader()
 
 # For each seed
-for seed_id in range(1, 4):
+for seed_id in range(1, seed_range):
     
     # Load list_IDs
-    list_IDs = list(np.load(os.path.join('list_IDs', 'seed' + str(seed_id), 
+    if args.dataset == 'CHUM':
+        list_IDs = list(np.load(os.path.join('list_IDs', 'seed' + str(seed_id), 
                                          args.kind_of_set + '_IDs.npy')))
-    
+    elif args.dataset == 'OpenKBP':
+        list_IDs = list(dataset.keys())
+    else:
+        raise ValueError("Unknown dataset. Handled datasets are CHUM and OpenKBP")
     
     # Debug
     #list_IDs = list_IDs[:2]
@@ -95,12 +138,6 @@ for seed_id in range(1, 4):
     average_row = {}
     for field_name in fields:
         average_row[field_name] = 0
-        
-    # Count the number of structures so we can compute the average
-    # on the right number of patients (some patients lack segmentations)
-    count_struct_m_dose = {}
-    for s_field_name in smax_fields + smean_fields:
-        count_struct_m_dose[s_field_name] = 0
     
     # For each patient
     for id in list_IDs:
@@ -111,62 +148,73 @@ for seed_id in range(1, 4):
         row['ID'] = id
         
         # Fetch the dose plan, prescribed dose and other masks
-        plan = unstandardize_rd(dataset[id]['dose'][()])
-        tumor_segmentation_bin = get_biggest_tv(dataset[id]['input'])
-        prescribed_dose = dataset[id]['prescribed_dose'][()]/100
+        plan = unstandardize_rd(dataset[id]['dose'][()], args.dataset) 
+        tumor_segmentation_bin, prescribed_dose = get_biggest_tv(dataset[id]['input'], args.dataset)
+        
+        if args.dataset == 'CHUM':
+            prescribed_dose = dataset[id]['prescribed_dose'][()]/100
+            plan *= dataset[id]['body']
+        elif args.dataset == 'OpenKBP':
+            dose_score_mask = dataset[id]['pdm'][()]
+            plan *= dose_score_mask[0, :, :, :, 0]
+        else:
+            raise ValueError("Unknown dataset. Handled datasets are CHUM and OpenKBP")
+            
         tumor_segmentation_gy = tumor_segmentation_bin * prescribed_dose
+        
+        #######################################################################
+        # Homogeneity 1 (Homogeneity)
+        #######################################################################
+        row['HI'] = homogeneity_1(plan, tumor_segmentation_gy)
+        average_row['HI'] += row['HI']
+
+        #######################################################################
+        # Homogeneity 2 (Dose homogeneity index DHI)
+        #######################################################################
+        row['H2'] = homogeneity_2(plan, tumor_segmentation_gy)
+        average_row['H2'] += row['H2']  
         
         #######################################################################
         # PTV coverage (DXX)
         #######################################################################        
         # D99
         coverage_value = 99
-        row['D99'] = ptv_coverage(plan, tumor_segmentation_gy, coverage_value)
+        row['D99'] = ptv_coverage(plan, tumor_segmentation_gy, coverage_value, prescribed_dose)
         average_row['D99'] += row['D99']
         
         # D98
         coverage_value = 98
-        row['D98'] = ptv_coverage(plan, tumor_segmentation_gy, coverage_value)
+        row['D98'] = ptv_coverage(plan, tumor_segmentation_gy, coverage_value, prescribed_dose)
         average_row['D98'] += row['D98']
 
         # D95
         coverage_value = 95
-        row['D95'] = ptv_coverage(plan, tumor_segmentation_gy, coverage_value)
-        average_row['D95'] += row['D95']    
+        row['D95'] = ptv_coverage(plan, tumor_segmentation_gy, coverage_value, prescribed_dose)
+        average_row['D95'] += row['D95']  
         
         #######################################################################
-        # Dmax (Maximum dose of the plan)
+        # Dmax (whole plan)
         #######################################################################
-        row['Dmax'] = np.max(plan) 
-        average_row['Dmax'] += row['Dmax']    
+        row['Dmax'] = max_dose_error_vs_prescribed(prescribed_dose, plan)
+        average_row['Dmax'] += row['Dmax'] 
         
         #######################################################################
-        # Structure max, mean dose (Dmax, Dmean)
+        # Conformity index CI
         #######################################################################
-        # Go along the masks stored between input[1] and input[20]
-        # as 0 is for the CT
-        for mask in range(1, dataset[id]['input'].shape[-1]):
-            
-            # if the mask is not empty
-            if dataset[id]['input'][:, :, :, mask].any():
-                row['Smax ' + str(mask)], row['Smean ' + str(mask)] = \
-                    structure_m_dose(plan, dataset[id]['input'][:, :, :, mask][()])
-                average_row['Smax ' + str(mask)] += row['Smax ' + str(mask)]
-                count_struct_m_dose['Smax ' + str(mask)] += 1 
-                average_row['Smean ' + str(mask)] += row['Smean ' + str(mask)]
-                count_struct_m_dose['Smean ' + str(mask)] += 1 
+        row['CI'] = conformity_index(plan, tumor_segmentation_gy, prescribed_dose)
+        average_row['CI'] += row['CI']
         
-        #######################################################################
-        # Homogeneity 1 (Homogeneity)
-        #######################################################################
-        row['H1'] = homogeneity_1(plan, tumor_segmentation_gy)
-        average_row['H1'] += row['H1']
-
-        #######################################################################
-        # Homogeneity 2 (Dose homogeneity index DHI)
-        #######################################################################
-        row['H2'] = homogeneity_2(plan, tumor_segmentation_gy)
-        average_row['H2'] += row['H2']         
+        ###############################################################################
+        # van't Riet conformation number
+        ###############################################################################
+        row["van't Riet"] = vant_riet(plan, tumor_segmentation_gy, prescribed_dose)
+        average_row["van't Riet"] += row["van't Riet"]
+        
+        ###############################################################################
+        # Dose spillage (R50)
+        ###############################################################################
+        row['R50'] = dose_spillage(plan, tumor_segmentation_bin, prescribed_dose)
+        average_row['R50'] += row['R50']
         
         # Write row
         metrics_gt_writer.writerow(row)
@@ -175,15 +223,10 @@ for seed_id in range(1, 4):
     # Compute average across patients
     ###########################################################################
     for field in average_row:
-        # Special treatment for structure max and mean dose since there is a chance
-        # to be lacking the segmentation
-        if field in smax_fields + smean_fields:
-            average_row[field] /= max(count_struct_m_dose[field], 1)
-        else:
-            average_row[field] /= len(list_IDs)
+        average_row[field] /= len(list_IDs)
         
     average_row['ID'] = 'Average for seed ' + str(seed_id)
     metrics_gt_writer.writerow(average_row)
     
-    average_across_seeds_writer.writerow({})
-    average_across_seeds_writer.writerow({})
+    metrics_gt_writer.writerow({})
+    metrics_gt_writer.writerow({})
